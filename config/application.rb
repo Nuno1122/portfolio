2029e0ab9fa8cbf54cdd705e92b4e932005cf442
Nuno <nonucode@gmail.com>
2023-03-15 17:39:34 +0900

    add:デフォルトのタイムゾーン変更 #26

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Portfolio
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
+     # デフォルトのタイムゾーンを日本に設定
+     config.time_zone = 'Tokyo'
+     config.active_record.default_timezone = :local
   end
 end

bdaeeff66ac5b996c18a709fb6f83b8712d54b2e
Nuno <nonucode@gmail.com>
2023-03-23 16:26:57 +0900

    add: ログイン機能 - UserSessionsController追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,19 @@
 class UserSessionsController < ApplicationController
-  def new
+  skip_before_action :require_login
+
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to root_path, notice: "ログインしました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, notice: "ログアウトしました"
   end
 end

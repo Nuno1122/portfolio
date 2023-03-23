06f66ac3540eac8614ad0fd466c30e241cc2758f
Nuno <nonucode@gmail.com>
2023-03-23 15:36:27 +0900

    add:generate時に不要なspecファイルを自動生成しないように設定

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,11 @@ module Portfolio
 
     config.generators do |g|
       g.helper false
-      g.skip_routes
+      g.skip_routes true
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
     end
 
     # デフォルトの言語を日本語に設定

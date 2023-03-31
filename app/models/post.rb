c2b9740fbef431f67a675d4514b73d926130244c
Nuno <nonucode@gmail.com>
2023-03-31 13:12:12 +0900

    add:UserモデルとPostモデルのアソシエーション、およびバリデーション定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :content, presence: true, length: { maximum: 140*2, message: '全角文字数上限は140文字です。' }, length: { maximum: 280, message: '半角文字数上限は280文字です。' }
 end

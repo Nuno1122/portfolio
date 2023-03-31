2238e6244950392da7fba6d1fba8cc2cb597ca9e
Nuno <nonucode@gmail.com>
2023-03-31 15:51:21 +0900

    add: annotateを使ってスキーマ情報を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :uuid             not null, primary key
+#  crypted_password :string
+#  email            :string           not null
+#  name             :string           not null
+#  salt             :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :posts, dependent: :destroy
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content    :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :uuid             not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do

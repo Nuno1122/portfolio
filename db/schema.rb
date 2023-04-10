ed66f9f5af6f2f25242228e6f1e6554c2a726449
Nuno <nonucode@gmail.com>
2023-04-10 12:24:32 +0900

    create:morning_activity_logsデータベース作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_409_082_450) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_031255) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'posts', force: :cascade do |t|
-    t.uuid 'user_id', null: false
-    t.text 'content', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_posts_on_user_id'
+  create_table "morning_activity_logs", force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.uuid "start_time_plan_id", null: false
+    t.datetime "started_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["start_time_plan_id"], name: "index_morning_activity_logs_on_start_time_plan_id"
+    t.index ["user_id"], name: "index_morning_activity_logs_on_user_id"
   end
 
-  create_table 'start_time_plans', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.uuid 'user_id', null: false
-    t.datetime 'start_time', null: false
-    t.datetime 'created_at', default: -> { 'CURRENT_TIMESTAMP' }, null: false
-    t.datetime 'updated_at', default: -> { 'CURRENT_TIMESTAMP' }, null: false
-    t.index ['user_id'], name: 'index_start_time_plans_on_user_id'
+  create_table "posts", force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table 'users', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'email', null: false
-    t.string 'crypted_password'
-    t.string 'salt'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
+  create_table "start_time_plans", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.datetime "start_time", null: false
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.index ["user_id"], name: "index_start_time_plans_on_user_id"
   end
 
-  add_foreign_key 'posts', 'users'
-  add_foreign_key 'start_time_plans', 'users'
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "morning_activity_logs", "start_time_plans", on_delete: :cascade
+  add_foreign_key "morning_activity_logs", "users"
+  add_foreign_key "posts", "users"
+  add_foreign_key "start_time_plans", "users"
 end

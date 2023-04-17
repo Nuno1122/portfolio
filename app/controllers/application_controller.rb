d46636cb174d6eab05407ebe3d4b88fa7feece19
Nuno <nonucode@gmail.com>
2023-04-17 13:53:53 +0900

    add:ログイン認証周りのリファクタリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   before_action :require_login
   add_flash_types :success, :info, :warning, :error
+
+  private
+
+  def not_authenticated
+    flash[:error] = 'ログインしてください'
+    redirect_to login_url
+  end
 end

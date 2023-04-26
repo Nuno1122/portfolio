692a5343b4644985ac85938f88f7602431d25e2c
Nuno <nonucode@gmail.com>
2023-04-26 18:10:26 +0900

    add:monthly_achievement.rb/新しい月に「達成カウント」がリセットするメソッドを追加

--- app/models/monthly_achievement.rb
+++ app/models/monthly_achievement.rb
@@ -20,8 +20,14 @@
 #
 class MonthlyAchievement < ApplicationRecord
   belongs_to :user
+  # 以下は、達成回数に関する定数を設定しています。
+  DEFAULT_ACHIEVED_COUNT = 0
   INCREMENT_VALUE = 1
   
+  #新しい月の達成回数（achieved_count）は0で初期化される
+  def self.initialize_new_month(user)
+    user.monthly_achievements.create!(year: Time.current.year, month: Time.current.month, achieved_count: DEFAULT_ACHIEVED_COUNT)
+  end
 
 # achieved_count に nil が入らないように設定。値が無い場合、to_i メソッドで 0 を返す。加算時には +1 される。
 def increment_achieved_count

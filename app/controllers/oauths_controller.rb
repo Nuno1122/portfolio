86274eeb5cbe2bab39da37b5117b08445dc84171
Nuno <nonucode@gmail.com>
2023-05-01 13:24:57 +0900

    fix:エラー修正(OauthsController にファイル名・コード変更)

---
+++ app/controllers/oauths_controller.rb
@@ -0,0 +1,35 @@
+class OauthsController < ApplicationController
+  skip_before_action :require_login
+  def oauth
+    login_at(auth_params[:provider])
+  end
+
+  def callback
+    provider = auth_params[:provider]
+
+    if auth_params[:denied].present?
+      redirect_to root_path, success: t('.denied.success')
+      return
+    end
+
+    begin
+      create_user_from(provider) unless (@user = login_from(provider))
+      redirect_to morning_activity_logs_path, success: t('.success')
+
+    rescue StandardError
+      redirect_to root_path, error: t('.fail')
+    end
+  end
+
+  private
+
+  def auth_params
+    params.permit(:provider, :denied, :oauth_token, :oauth_verifier)
+  end
+
+  def create_user_from(provider)
+    @user = create_from(provider)
+    reset_session
+    auto_login(@user)
+  end
+end

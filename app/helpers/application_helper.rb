f54dfaecbcb2fdf639f24709a2f5b5320b3fbeac
Nuno <nonucode@gmail.com>
2023-05-03 20:29:27 +0900

    “add:FontAwesomeの導入”

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
-end
+    def icon(icon_style, icon_name, size: nil)
+      classes = ["fa-#{icon_style}", "fa-#{icon_name}"]
+      classes << "text-#{size}" if size
+      tag.i(class: classes)
+    end
+  end
+  

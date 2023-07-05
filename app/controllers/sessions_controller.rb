5916ad63d523cef4cb83941c30ca795370866bf5
Nuno <nonucode@gmail.com>
2023-07-05 16:57:50 +0900

    add:フラッシュメッセージを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     def create
       user = User.find_or_create_from_auth(request.env["omniauth.auth"])
       session[:user_id] = user.id  # `uid`ではなく`id`を使用する
-      redirect_to morning_activity_logs_path
+      redirect_back_or_to morning_activity_logs_path, success: t('.success')
     end
     
   
